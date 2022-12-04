12d08d3e3a9124c35c2848d15d97623a6bf1424e
C <jotapedeside@gmail.com>
2022-12-04 17:38:02 -0300

    feature(booking): booking controller created and indexed

---
+++ src/controllers/bookings-controller.ts
@@ -0,0 +1,51 @@
+import { AuthenticatedRequest } from "@/middlewares";
+import bookingService from "@/services/bookings-service";
+import { Response } from "express";
+import httpStatus from "http-status";
+
+export async function getBookings(req: AuthenticatedRequest, res: Response) {
+  const userId = req.userId;
+
+  try {
+    const bookings = await bookingService.getBookings(userId);
+
+    return res.status(httpStatus.OK).send(bookings);
+  } catch (error) {
+    return res.sendStatus(httpStatus.NOT_FOUND);
+  }
+}
+
+export async function postBooking(req: AuthenticatedRequest, res: Response) {
+  const userId = req.userId;
+  const roomId: number = req.body.roomId;
+
+  try {
+    const booking = await bookingService.postBooking(userId, roomId);
+
+    return res.status(httpStatus.OK).send(booking);
+  } catch (error) {
+    if (error.name === "NotFoundError") return res.sendStatus(httpStatus.NOT_FOUND);
+    if (error.name === "ForbiddenError") return res.sendStatus(httpStatus.FORBIDDEN);
+    if (error.name === "UnauthorizedError") return res.sendStatus(httpStatus.UNAUTHORIZED);
+
+    return res.sendStatus(error);
+  }
+}
+
+export async function putBooking(req: AuthenticatedRequest, res: Response) {
+  const userId = req.userId;
+  const bookingId = Number(req.params.bookingId);
+  const roomId: number = req.body.roomId;
+
+  try {
+    const booking = await bookingService.putBooking(userId, bookingId, roomId);
+
+    return res.status(httpStatus.OK).send(booking);
+  } catch (error) {
+    if (error.name === "NotFoundError") return res.sendStatus(httpStatus.NOT_FOUND);
+    if (error.name === "ForbiddenError") return res.sendStatus(httpStatus.FORBIDDEN);
+    if (error.name === "UnauthorizedError") return res.sendStatus(httpStatus.UNAUTHORIZED);
+
+    return res.sendStatus(httpStatus.NOT_FOUND);
+  }
+}
